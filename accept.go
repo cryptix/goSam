c9345cbcdbd14b85f3e462228dbd3a5825199939
Henry <cryptix@riseup.net>
2014-02-12 22:14:19 +0100

    First stab at STREAM ACCEPT

---
+++ accept.go
@@ -0,0 +1,34 @@
+package goSam
+
+import (
+	"fmt"
+	"net"
+)
+
+// creates a new Client and Accept a connection on it
+func (c *Client) Accept() (net.Conn, error) {
+	id, newAddr, err := c.CreateStreamSession("")
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("NewAddr:", newAddr)
+
+	newC, err := NewDefaultClient()
+	if err != nil {
+		return nil, err
+	}
+
+	if c.verbose {
+		newC.ToggleVerbose()
+	}
+
+	resp, err := newC.StreamAccept(id)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Accept Resp:", resp)
+
+	return newC.SamConn, nil
+}
--- stream.go
+++ stream.go
@@ -24,3 +24,23 @@ func (c *Client) StreamConnect(id int32, dest string) (err error) {
 
 	return nil
 }
+
+// Ask SAM to accept a TCP-Like connection
+func (c *Client) StreamAccept(id int32) (r *Reply, err error) {
+
+	r, err = c.sendCmd(fmt.Sprintf("STREAM ACCEPT ID=%d SILENT=false", id))
+	if err != nil {
+		return nil, err
+	}
+
+	if r.Topic != "STREAM" || r.Type != "STATUS" {
+		return nil, fmt.Errorf("Unknown Reply: %+v\n", r)
+	}
+
+	result := r.Pairs["RESULT"]
+	if result != "OK" {
+		return nil, ReplyError{result, r}
+	}
+
+	return r, nil
+}
