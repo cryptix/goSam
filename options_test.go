c30f373ebe6eaf457e25bbc5981465a0aa6ef792
idk <problemsolver@openmailbox.org>
2018-05-03 17:37:10 -0400

    forgot to gofmt

---
+++ options_test.go
@@ -0,0 +1,30 @@
+package goSam
+
+import "testing"
+
+func optaddr(t *testing.T) {
+	client, err := NewClientFromOptions(SetAddr("127.0.0.1"))
+	if err != nil {
+		t.Fatalf("NewDefaultClient() Error: %q\n", err)
+	}
+	if err := client.Close(); err != nil {
+		t.Fatalf("client.Close() Error: %q\n", err)
+	}
+}
+
+func optport(t *testing.T) {
+	client, err := NewClientFromOptions(SetPort("7656"))
+	if err != nil {
+		t.Fatalf("NewDefaultClient() Error: %q\n", err)
+	}
+	if err := client.Close(); err != nil {
+		t.Fatalf("client.Close() Error: %q\n", err)
+	}
+}
+
+func TestOptionsHello(t *testing.T) {
+	ConnDebug = true
+	setup(t)
+	optaddr(t)
+	optport(t)
+}
