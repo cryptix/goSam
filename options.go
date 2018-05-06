cc0203f98251519cff568bf8a7cad9f2d151e672
idk <problemsolver@openmailbox.org>
2018-05-05 20:23:06 -0400

    SetPort now accepts either a string or an integer port.

--- options.go
+++ options.go
@@ -16,12 +16,12 @@ func SetAddr(s ...interface{}) func(*Client) error {
 				split := strings.SplitN(v, ":", 2)
 				if len(split) == 2 {
 					if i, err := strconv.Atoi(split[1]); err == nil {
-                        if i < 65536 {
-    						c.addr = split[0]
-                            c.port = split[1]
-                        }else{
-                            return fmt.Errorf("Invalid port")
-                        }
+						if i < 65536 {
+							c.addr = split[0]
+							c.port = split[1]
+						} else {
+							return fmt.Errorf("Invalid port")
+						}
 					} else {
 						return fmt.Errorf("Invalid port; non-number")
 					}
@@ -34,20 +34,20 @@ func SetAddr(s ...interface{}) func(*Client) error {
 		} else if len(s) == 2 {
 			switch v := s[1].(type) {
 			case int:
-                if v < 65536 {
-                    c.addr = s[0].(string)
-                    c.port = strconv.Itoa(v)
-                }else{
-                    return fmt.Errorf("Invalid port")
-                }
+				if v < 65536 {
+					c.addr = s[0].(string)
+					c.port = strconv.Itoa(v)
+				} else {
+					return fmt.Errorf("Invalid port")
+				}
 			case string:
 				if i, err := strconv.Atoi(s[1].(string)); err == nil {
-                    if i < 65536 {
-                        c.addr = s[0].(string)
-                        c.port = s[1].(string)
-                    }else{
-                        return fmt.Errorf("Invalid port")
-                    }
+					if i < 65536 {
+						c.addr = s[0].(string)
+						c.port = s[1].(string)
+					} else {
+						return fmt.Errorf("Invalid port")
+					}
 				} else {
 					return fmt.Errorf("Invalid port; non-number")
 				}
@@ -68,16 +68,28 @@ func SetHost(s string) func(*Client) error {
 	}
 }
 
-func SetPort(s string) func(*Client) error {
+func SetPort(s interface{}) func(*Client) error {
 	return func(c *Client) error {
-		port, err := strconv.Atoi(s)
-		if err != nil {
-			return fmt.Errorf("Invalid port; non-number")
-		}
-		if port < 65536 && port > -1 {
-			c.port = s
-			return nil
-		} else {
+		switch v := s.(type) {
+		case string:
+			port, err := strconv.Atoi(v)
+			if err != nil {
+				return fmt.Errorf("Invalid port; non-number")
+			}
+			if port < 65536 && port > -1 {
+				c.port = v
+				return nil
+			} else {
+				return fmt.Errorf("Invalid port")
+			}
+		case int:
+			if v < 65536 && v > -1 {
+				c.port = strconv.Itoa(v)
+				return nil
+			} else {
+				return fmt.Errorf("Invalid port")
+			}
+		default:
 			return fmt.Errorf("Invalid port")
 		}
 	}
